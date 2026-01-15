b22309258c0c2daeee90821a7e29ea9c3b5c0f99
Rojaline Sahoo <srojaline48@gmail.com>
2026-01-15 19:02:58 +0530

    Improved the backend authentication

--- client/src/utils/delj.js
+++
@@ -1,7 +0,0 @@
-export const authHeader = () => {
-  const token = localStorage.getItem("token");
-  return {
-    Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json"
-  };
-};
