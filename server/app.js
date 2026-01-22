f557b0599abed2c154c40c9df826fa8d7780ba49
Rojaline Sahoo <srojaline48@gmail.com>
2026-01-22 17:23:47 +0530

    setup env config and server port

--- server/app.js
+++ server/app.js
@@ -1,29 +1,11 @@
 import express from "express";
-import cors from "cors";
-import authRoutes from "./routes/authRoutes.js";
-import menuRoutes from "./routes/menuRoutes.js";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/menu", menuRoutes);
 app.get("/", (req, res) => {
-  res.send("API Running");
+  res.send("Smart Mess Management System API is running");
 });
-import { protect } from "./middleware/authMiddleware.js";
-
-app.get("/api/test", protect, (req, res) => {
-  res.json({
-    message: "Token is valid",
-    user: req.user
-  });
-});
-
-import mealRoutes from "./routes/mealRoutes.js";
-
-app.use("/api/meals", mealRoutes);
 
 export default app;
