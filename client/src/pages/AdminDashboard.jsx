648f48e43b78acbfe7db1813b116b469853a87e0
Rojaline Sahoo <srojaline48@gmail.com>
2026-01-27 01:35:37 +0530

    feat: admin dashboard live tomorrow meal count

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,5 +1,54 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function AdminDashboard() {
-  return <h2>Admin Dashboard</h2>;
+  const [mealCount, setMealCount] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCount = async () => {
+      try {
+        const res = await api.get("/admin/meals/tomorrow/count");
+        setMealCount(res.data);
+      } catch (err) {
+        console.error("Failed to fetch meal count");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCount();
+  }, []);
+
+  if (loading) return <p>Loading admin dashboard...</p>;
+
+  return (
+    <div style={{ padding: "16px", maxWidth: "600px" }}>
+      <h2>Admin Dashboard</h2>
+
+      <div
+        style={{
+          border: "1px solid #ccc",
+          padding: "16px",
+          marginTop: "16px",
+        }}
+      >
+        <h3>Tomorrow’s Meal Count</h3>
+
+        <p>
+          <strong>Total Meals:</strong> {mealCount.count}
+        </p>
+
+        <p>
+          <strong>Status:</strong>{" "}
+          {mealCount.status === "TENTATIVE" ? "🟡 Tentative" : "🟢 Final"}
+        </p>
+
+        <small>Date: {mealCount.date}</small>
+      </div>
+    </div>
+  );
 }
 
 export default AdminDashboard;
